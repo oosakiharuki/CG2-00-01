70679f1c793ba70bfac3df09cdfd93271c971b8a
oosakiharuki <k023g0035@g.neec.ac.jp>
2024-10-21 16:04:55 +0900

    03_02完了

---
+++ WinApp.h
@@ -0,0 +1,18 @@
+#include<Windows.h>
+
+#pragma once
+
+class WinApp
+{
+public://静寂メンバ関数
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+public://メンバ関数
+	void Initialize();
+	void Update();
+
+
+private:
+	WNDCLASS wc{};
+
+};
