218eedfa89fcdcde556881973709b2f49f66a864
oosakiharuki <k023g0035@g.neec.ac.jp>
2024-12-02 15:02:31 +0900

    GE3_05_02完了

--- SpriteCommon.h
+++ SpriteCommon.h
@@ -1,8 +1,29 @@
 #pragma once
 #include "DirectXCommon.h"
 
-class SpriteCommon{
+class SpriteCommon {
 public:
-	void Initialize();
+	void Initialize(DirectXCommon* dxCommon);
+	DirectXCommon* GetDirectXCommon()const { return dxCommon_; }
+
+	void Command();
+
+private:
+	//PSO
+	void RootSignature();
+	void GraphicsPipeline();
+
+
+	DirectXCommon* dxCommon_;
+
+	//RootSignature
+	D3D12_ROOT_SIGNATURE_DESC descriptionRootSignature{};
+
+
+	//バイナリを元に生成
+	Microsoft::WRL::ComPtr < ID3D12RootSignature> rootSignature = nullptr;
+
+	//PSO
+	Microsoft::WRL::ComPtr < ID3D12PipelineState> graphicsPipelineState = nullptr;
 
 };
